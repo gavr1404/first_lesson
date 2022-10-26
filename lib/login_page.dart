a2e648ed39dfa9bcc30f0ab1f6230e8c643327f6
Gavr1404 <gavr140484@gmail.com>
2022-10-27 02:36:47 +0300

    Commit init

---
+++ lib/login_page.dart
@@ -0,0 +1,209 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:first_lesson/forgot_page.dart';
+import 'package:first_lesson/home_page.dart';
+import 'package:first_lesson/register_now.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => LoginState();
+}
+
+class LoginState extends State<LoginPage> {
+  bool isRememberMe = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 92, 194, 82),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.phone_android,
+              color: Colors.white,
+              size: 70,
+            ),
+            SizedBox(height: 40),
+
+            // hello
+            Text("Hello and start...",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 35,
+                )),
+
+            SizedBox(height: 5),
+
+            // next hello
+            Text(
+              "log in please",
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+
+            SizedBox(height: 60),
+
+            // login
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(15)),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 5),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'login',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            // password
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(15)),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 5),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'password',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            // remember my
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Container(
+                child: Row(children: [
+                  Theme(
+                    data: ThemeData(unselectedWidgetColor: Colors.white),
+                    child: Checkbox(
+                      value: isRememberMe,
+                      checkColor: Colors.green,
+                      activeColor: Colors.white,
+                      onChanged: (value) {
+                        setState(() {
+                          isRememberMe = value!;
+                        });
+                      },
+                    ),
+                  ),
+                  Text(
+                    'Remember me',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ]),
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            // button sign in
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 100),
+              child: FloatingActionButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const HomePage()));
+                },
+                child: Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Colors.green[800],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Text(
+                    "Sing in",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 10,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            // Text(
+            //   "Sign in",
+            //   style: TextStyle(
+            //     color: Colors.white,
+            //     fontWeight: FontWeight.bold,
+            //     fontSize: 15,
+            //   ),
+            // ),
+
+            SizedBox(height: 20),
+
+            //register new member
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 30),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const RegisterPage()));
+                    },
+                    child: Text("Register now"),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 150),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const ForgotPassword()));
+                    },
+                    child: Text("Forgot password?"),
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // bool isRememberMe = false;
+
+}
